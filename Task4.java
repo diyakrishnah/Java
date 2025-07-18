879777205b74b4b610018c4fbb705b3d938f9fa7
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2025-07-18 10:04:45 +0530

    Add files via upload

---
+++ Task4.java
@@ -0,0 +1,24 @@
+/*AUTHOR:DIYA KRISHNA
+ * DATE:18/7/2025
+ */
+class Book{
+		String title;
+		String Author;
+		double price;
+		Book(){
+			title="Unknown";
+			Author="Not Assigned";
+			price=0.0;
+		}
+		public void displaydetails() {
+			System.out.println("Title:\t"+title);
+			System.out.println("Author:\t"+Author);
+			System.out.println("Price:\t"+price);
+		}
+		}
+public class Task4 {
+	public static void main(String[] args) {
+		Book books =new Book();
+		books.displaydetails();
+	}
+}
